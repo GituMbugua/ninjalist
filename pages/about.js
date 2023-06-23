9de15258785d1c6bbd39a02282f1392e9be8a63e
Gitu Mbugua <gmbugua38@gmail.com>
2023-06-23 17:46:46 +0300

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem nemo delectus necessitatibus ipsa repudiandae dolorum quidem beatae, facere voluptas obcaecati explicabo impedit et, officiis eum quam ipsam. Vel, facilis commodi?</p>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem nemo delectus necessitatibus ipsa repudiandae dolorum quidem beatae, facere voluptas obcaecati explicabo impedit et, officiis eum quam ipsam. Vel, facilis commodi?</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
